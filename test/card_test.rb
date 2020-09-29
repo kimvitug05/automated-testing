1138b1c3914ecd5afd81970df382025451af730e
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-28 19:06:27 -0700

    add test that accesses Card's .suit

--- test/card_test.rb
+++ test/card_test.rb
@@ -68,7 +68,12 @@ describe Card do
     end
 
     it "Can retrieve the value of the card using a `.suit`." do
-      # ensure that `.suit works as expected returning the symbol of the suit`
+      [:hearts, :spades, :clubs, :diamonds].each do |suit|
+        (1..13).each do |value|
+          card = Card.new(value, suit)
+          expect(card.suit).must_equal suit
+        end
+      end
 
     end
   end
