43b87257ad41d5cafc13fe02d5f15a80970754f5
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-28 19:55:10 -0700

    add test for draw method

--- test/deck_test.rb
+++ test/deck_test.rb
@@ -1,12 +1,19 @@
 require_relative 'test_helper'
 
 describe Deck do
+  before do
+    @deck = Deck.new
+  end
+
   describe "constructor" do
-    before do
-      @deck = Deck.new
-    end
     it "can create a new Deck" do
       expect(@deck).must_be_instance_of Deck
+    end
+  end
+
+  describe "draw method" do
+    it "must respond to draw method" do
+      expect(@deck).must_respond_to :draw
     end
   end
 end
