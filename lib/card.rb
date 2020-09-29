675d958b0160087041c018a994690ceb9e919958
Kimberly Vitug <kim_vitug05@yahoo.com>
2020-09-28 18:25:35 -0700

    add raise ArgumentError for invalid suits and values

--- lib/card.rb
+++ lib/card.rb
@@ -1,5 +1,5 @@
+VALID_SUITS = [:hearts, :spades, :clubs, :diamonds]
 
-# card.rb
 
 class Card
   attr_reader :value, :suit
@@ -7,12 +7,12 @@ class Card
   def initialize(value, suit)
     @value = value
     @suit = suit
-    
 
+    raise ArgumentError.new("Not a valid value.") if value < 1 || value > 13
+    raise ArgumentError.new("Not a valid suit.") if !VALID_SUITS.include?(suit)
   end
 
   def to_s
     return "#{value} of #{suit.to_s}"
   end
-
 end
